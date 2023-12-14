c5fd8ccb5d60f66c3fef44a77895f6bd0654c611
munene-m <munenenjue18@gmail.com>
2023-12-14 18:00:24 +0300

    chore: initial commit

--- src/components/GetWeather.tsx
+++ src/components/GetWeather.tsx
@@ -8,20 +8,21 @@ const WeatherInfo = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
 
-  const getData = async () => {
-    function handleError(errorData: ErrorData) {
-      // Check the structure of the error response and handle each error type
-      if (errorData && errorData.errorMessage) {
-        // Handle specific error type
-        setError(errorData.errorMessage);
-      } else if (errorData && errorData.error) {
-        // Handle another specific error type
-        setError(errorData.error);
-      } else {
-        // Handle other types of errors based on the structure of errorData
-        setError("An unknown error occured");
-      }
+  function handleError(errorData: ErrorData) {
+    // Check the structure of the error response and handle each error type
+    if (errorData && errorData.errorMessage) {
+      // Handle specific error type
+      setError(errorData.errorMessage);
+    } else if (errorData && errorData.error) {
+      // Handle another specific error type
+      setError(errorData.error);
+    } else {
+      // Handle other types of errors based on the structure of errorData
+      setError("An unknown error occured");
     }
+  }
+
+  const getData = async () => {
     //fetch from the weather API
     try {
       const response = await fetch("https://api.example.com/weather");
@@ -38,26 +39,26 @@ const WeatherInfo = () => {
       // Handle unexpected errors
       setError(error);
     }
-
-    useEffect(() => {
-      // Fetch weather data from API here and handle errors
-      getData();
-    }, []);
-
-    return (
-      <div>
-        {error ? (
-          <p>Error: {error}</p>
-        ) : weatherData ? (
-          <div>
-            <h2>Weather Information</h2>
-            {/* Display weather data */}
-          </div>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    );
   };
+  useEffect(() => {
+    // Fetch weather data from API here and handle errors
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {error ? (
+        <p>Error: {error}</p>
+      ) : weatherData ? (
+        <div>
+          <h2>Weather Information</h2>
+          {/* Display weather data */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
+
 export default WeatherInfo;
